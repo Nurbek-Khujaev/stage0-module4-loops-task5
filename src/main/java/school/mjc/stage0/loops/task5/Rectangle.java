c62d6865ec5fd8f2453b4457942440d02a27389d
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-11-08 00:39:49 +0500

    bugs fixed

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -6,7 +6,7 @@ public class Rectangle {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < length; j++) {
 
-                if (i == 0 || i == length - 1 || j == 0 || j == height - 1) {
+                if (i == 0 || i == height - 1 || j == 0 || j == length - 1) {
                     System.out.print("8");
                 } else {
                     System.out.print(" ");
